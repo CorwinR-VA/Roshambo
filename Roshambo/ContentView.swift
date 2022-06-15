907952311b616df4529a581a22bc022ba1621f1c
Corwin Rainier <corwinrainier89@gmail.com>
2022-06-15 13:46:13 -0300

    Initial Commit

---
+++ Roshambo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Roshambo
+//
+//  Created by Corwin Rainier on 6/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Roshambo/RoshamboApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoshamboApp.swift
+//  Roshambo
+//
+//  Created by Corwin Rainier on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct RoshamboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
